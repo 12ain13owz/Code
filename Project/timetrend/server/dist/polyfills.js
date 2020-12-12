f427b3a66cf834eb785f6d3b450b411b26667afa
12ain13owz <touchfn@gmail.com>
2020-12-12 14:06:46 +0700

    fix district
    update subdistrict

--- Project/timetrend/server/dist/polyfills.js
+++ Project/timetrend/server/dist/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\src\timetrend\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! D:\Code\Project\timetrend\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
--- Project/timetrend/server/dist/styles.js
+++ Project/timetrend/server/dist/styles.js
@@ -58,8 +58,8 @@ ___CSS_LOADER_EXPORT___.push([module.i, "* {\n  box-sizing: border-box;\n}\n\nht
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\src\timetrend\node_modules\@angular\material\prebuilt-themes\indigo-pink.css */"8jWO");
-module.exports = __webpack_require__(/*! D:\src\timetrend\src\styles.scss */"+EN/");
+__webpack_require__(/*! D:\Code\Project\timetrend\node_modules\@angular\material\prebuilt-themes\indigo-pink.css */"8jWO");
+module.exports = __webpack_require__(/*! D:\Code\Project\timetrend\src\styles.scss */"+EN/");
 
 
 /***/ }),
